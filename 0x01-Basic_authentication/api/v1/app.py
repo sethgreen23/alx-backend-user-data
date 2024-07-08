44a4af9223ec6c6ff366bfecbcf521f23260c83a
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-08 14:20:23 +0100

    task 3

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,6 +20,15 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def unauthorized_handler(error) -> str:
+    """ Unauthorized handler"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+@app.errorhandler(403)
+def not_allowed(error) -> str:
+    """ Not allowed handler"""
+    return jsonify({"error": "Forbidden"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
