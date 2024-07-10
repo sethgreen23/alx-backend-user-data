300271599a552acc583a6e78db6f14964a77f1d6
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-11 00:33:27 +0100

    task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,17 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Class Session Auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create session"""
+        if not user_id or not isinstance(user_id, str):
+            return None
+        id = str(uuid4())
+        self.user_id_by_session_id[id] = user_id
+        return id
