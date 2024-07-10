7867a487f9f03b40d8efae67c5adb100df02c1f6
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-11 00:15:32 +0100

    task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Session Auth module"""
+from flask import request
+from typing import List, TypeVar
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Class Session Auth"""
+    pass
