aba97e75b889a9ca402ab9c07a73b379d995f3c9
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-11 21:00:05 +0100

    task 10

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,55 +1,52 @@
 #!/usr/bin/env python3
-""" Session DB Auth module"""
-from flask import request
-from typing import List, TypeVar
+"""
+SessionDBAuth class to manage API authentication
+"""
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
 from os import getenv
 from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
-    """ Session DB Auth class"""
+    """SessionExpAuth class to manage API authentication
+    """
+
     def create_session(self, user_id=None):
-        """ Create Session DB Auth"""
-        session_id = super().create_session(user_id)
-        if not session_id:
-            return None
-        try:
-            from models.user_session import UserSession
-            user_session = UserSession({'user_id': user_id,
-                                        'session_id': session_id})
-            user_session.save()
-        except Exception:
-            return None
-        return session_id
+        """Create session
+        """
+        if user_id:
+            session_id = super().create_session(user_id)
+            us = UserSession(user_id=user_id, session_id=session_id)
+            us.save()
+            UserSession.save_to_file()
+            return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """ User id for session id"""
-        try:
-            from models.user_session import UserSession
-            users = UserSession.search({'session_id': session_id})
-            if not users:
-                return None
-            user_id = users[0].user_id
-            return user_id
-        except Exception:
-            return None
-
-    def destroy_session(self, request=None) -> bool:
-        """ Destroy session"""
-        if not request:
-            return False
-        session_id = self.session_cookie(request)
+        """Get user ID from session
+        """
         if not session_id:
-            return False
-        try:
-            from models.user_session import UserSession
-            user_sessions = UserSession.search({'session_id': session_id})
-            if not user_sessions:
+            return None
+        UserSession.load_from_file()
+        users = UserSession.search({'session_id': session_id})
+        for u in users:
+            delta = timedelta(seconds=self.session_duration)
+            if u.created_at + delta < datetime.now():
+                return None
+            return u.user_id
+
+    def destroy_session(self, request=None):
+        """Delete the user session / log out
+        """
+        if request:
+            session_id = self.session_cookie(request)
+            if not session_id:
                 return False
-            user_session = user_sessions[0]
-            user_session.remove()
-            self.user_id_by_session_id.pop(session_id)
-            return True
-        except Exception:
-            return False
+            if not self.user_id_for_session_id(session_id):
+                return False
+            users = UserSession.search({'session_id': session_id})
+            for u in users:
+                u.remove()
+                UserSession.save_to_file()
+                return True
+        return False
